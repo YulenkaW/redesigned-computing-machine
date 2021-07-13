e3beea9d3d38f5c2dd7f5bf8761ac5f5508305cd
YulenkaW <83892913+YulenkaW@users.noreply.github.com>
2021-07-12 21:45:44 -0400

    Add files via upload

---
+++ ElectricVehicle.java
@@ -0,0 +1,99 @@
+
+package project3.pkg2;
+
+
+public class ElectricVehicle {
+
+    //private static Object keyboard;
+ String Make;
+ String Model;
+ String Color;
+ double KilometersPerKilowattHour;
+ int MaxKilowattHoursInBattery;
+ double CurrentKilowatHoursInBattery;
+//double kilometersToDrive;
+
+
+    
+    public ElectricVehicle(String Make, String Model, String Color, double KilometersPerKw, int CurrentKilowatHoursInBattery,int MaxKilowattHoursInBattery){
+    this.Model=Model;
+    this.Make=Make;
+    this.Color=Color;
+    this.KilometersPerKilowattHour=KilometersPerKw;
+    this.CurrentKilowatHoursInBattery=10;
+     this.MaxKilowattHoursInBattery=MaxKilowattHoursInBattery ;
+    }
+    
+
+@Override
+    public String toString() {
+        String wht1;
+        wht1=String.format("%s %s %s ", Color, Make, Model);
+        String wth2=String.format("%.1f  %d  out of %d\n", KilometersPerKilowattHour,CurrentKilowatHoursInBattery, MaxKilowattHoursInBattery );
+        
+        return wht1+wth2;
+                
+        
+    }
+     public void charge(int charge ){
+         CurrentKilowatHoursInBattery+=charge;
+        if (CurrentKilowatHoursInBattery>MaxKilowattHoursInBattery){
+           CurrentKilowatHoursInBattery= MaxKilowattHoursInBattery;
+        }
+        if (CurrentKilowatHoursInBattery<0){
+            System.out.println("Your battery is 0, charge it ");
+        }
+     }
+          
+      public boolean kilometersToDrive(double correctKilometersToDrive){
+          
+       correctKilometersToDrive=CurrentKilowatHoursInBattery-(correctKilometersToDrive/KilometersPerKilowattHour);
+       if (correctKilometersToDrive< 0||                               
+               System.out.println(""
+           
+       }
+       
+    
+    
+    
+     }
+     
+    public String getMake() {
+        return Make;
+}
+    public void setMake(String Make) {
+        this.Make=Make;
+}
+    public String getModel(){
+        return Model;
+    }
+    public void setModel(String Model){
+        this.Model=Model;
+    }
+        
+    public String getColor(){
+        return Color;
+    }
+    public void setColor(String Color){
+        this.Color=Color;
+    }
+     public double getKilometersPerKilowattHour(){
+        return KilometersPerKilowattHour;
+    }
+     public void setKilometersPerKilowattHour(double KilometersPerKilowattHour){
+         this.KilometersPerKilowattHour=KilometersPerKilowattHour;
+     }
+     public int getMaxKilowattHoursInBattery(){
+         return MaxKilowattHoursInBattery;
+}
+     public void setMaxKilowattHoursInBattery(int MaxKilowattHoursInBattery){
+         this.MaxKilowattHoursInBattery=MaxKilowattHoursInBattery;
+     }  
+     public double getCurrentKilowatHoursInBattery(){
+         return CurrentKilowatHoursInBattery;
+}
+     public void setCurrentKilowatHoursInBattery(double CurrentKilowatHoursInBattery){
+         this.CurrentKilowatHoursInBattery=CurrentKilowatHoursInBattery;
+     }  
+
+}
---
+++ Project32.java
@@ -0,0 +1,37 @@
+
+package project3.pkg2;
+
+
+
+public class Project32 {
+
+
+   
+    public static void main(String[] args) {
+      ElectricVehicle Ford=new ElectricVehicle("Ford","Mustang", "White",3500, 350, 1000  ); 
+      System. out.println(Ford);
+      Ford.charge(1000);
+     System. out.println(Ford);
+     
+    
+     
+    }
+    
+   
+    
+   
+    
+    
+    
+    
+    
+    
+}
+
+   // private static void KilometersToDrive() {
+     //   throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    //}
+
+    //ElectricVehicle newFord=new ElectricVehicle("Ford","Mustang", "White",3500, 350, 1000);
+    
+   
